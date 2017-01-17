8655ca4477abc526ab7a7b6bc649bf82b663160e
Scott Chao <svinets@gmail.com>
2017-01-16 17:18:04 -0800

    Restructure models for separate schema

--- server/reviews/reviewModel.js
+++ server/reviews/reviewModel.js
@@ -1,16 +1,19 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-module.exports = {
-  reviewSchema: new mongoose.Schema({
-    author: String,
-    title: {type: String, required: true},
-    score: {type: Number, required: true},
-    votes: Number,
-    text: {type: String, required: true}
-  }),
+var reviewSchema = new mongoose.Schema({
+  author: String,
+  title: {type: String, required: true},
+  score: {type: Number, required: true},
+  votes: Number,
+  text: {type: String, required: true}
+});
 
-  canvasSchema: new mongoose.Schema({
-    URI: String
-  })
+var canvasSchema = new mongoose.Schema({
+  URI: String
+});
+
+module.exports = {
+  reviewModel: mongoose.model('Review', reviewSchema),
+  canvasModel: mongoose.model('Canvas', canvasSchema)
 }
