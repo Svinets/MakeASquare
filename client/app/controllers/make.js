69902095ef7cdbc3f50150d65e63abd32bd18ef1
Scott Chao <svinets@gmail.com>
2017-01-17 11:39:56 -0800

    Drawing time limited to 30 seconds and auto-submits canvas

--- client/app/controllers/make.js
+++ client/app/controllers/make.js
@@ -1,26 +1,45 @@
 angular.module('draw.make', [])
 
-.controller('makeController', function($scope, $location, Squares) {
+.controller('makeController', function($scope, $location, $timeout, $window, Squares) {
   $scope.message = 'Is this going to work?';
+  $scope.timer = 30;
+
+  $scope.back = function() {
+    $location.path('/squares');
+  }
 
   $scope.getWord = function() {
     $scope.randomWord = Squares.randomWord();
-  }  
+  }
+
+  $scope.startTimer = function() {
+    $scope.timer--;
+    timeOut = $timeout($scope.startTimer, 1000);
+  }
+
+  var timeOut = $scope.startTimer;
 
   $scope.submit = function() {
+    var word = $scope.randomWord;
     setTimeout(function() {
-      console.log(dataURI);
       Squares.makeSquare({
         artist: 'anon', //the name of the artist
         URI: dataURI, //the dataURI of the canvas
-        subject: 'rabbit', //the subject of the image
+        subject: word, //the subject of the image
         guesses: [] //an array containing all guesses directed towards the image
       });
       $location.path('/squares');
     }, 0);
   }
 
-  $scope.back = function() {
-    $location.path('/squares');
+  function logTime() {//the dataURI is being pushed to window in sketch.js
+    if ($scope.timer === 0) {
+      $window.dataURI = document.querySelector('#sketch').toDataURL().toString();
+      $scope.submit();
+      $timeout.cancel(timeOut);
+    }
   }
+
+  $scope.$watch('timer', logTime);
+
 });
